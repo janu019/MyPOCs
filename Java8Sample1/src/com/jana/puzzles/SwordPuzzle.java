8fd44cdcf165e3c902a22e3e638c04f26e45780e
janareddy <janareddy@overstock.com>
2017-09-21 16:49:02 -0600

    Problem statement well documented

--- Java8Sample1/src/com/jana/puzzles/SwordPuzzle.java
+++ Java8Sample1/src/com/jana/puzzles/SwordPuzzle.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 /**
  * Problem statement:
- * 
+ *
  * 100 people standing in a circle in an order 1 to 100. No.1 has a sword. He kills next person (i.e. no. 2) and gives
- * sword to next to next (i.e no.3). All person does the same until only 1 survives. Which number survives at the last?
- * 
- * 
+ * sword to next to next (i.e no.3). This will go on (Guy kills next one and hand over sword to the next person who is alive), this will continue until only 1 survives.
+ * Finding the person number who survives at the end is the problem.
+ *
+ *
  * @author janareddy
  *
  */
@@ -49,7 +50,7 @@ public class SwordPuzzle {
    * This is a recursive method which will take all the alive people and start killing the neighbor and handle the sword
    * to next guy. This creates new list of alive people for the next round with killFirstPerson. if killFirstPerson is
    * true then it skips add the first guy because last guy in the previous had the sword.
-   * 
+   *
    * @param people
    * @param killFirstPerson
    * @param round
